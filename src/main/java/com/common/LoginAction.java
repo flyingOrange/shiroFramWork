9912210e43c352610392eb7c850e445aa52427e2
flyingOrange <370686124@qq.com>
2019-06-04 20:33:28 +0800

    auth

---
+++ src/main/java/com/common/LoginAction.java
@@ -0,0 +1,34 @@
+package com.common;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.subject.Subject;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginAction {
+
+    @RequestMapping("login")
+    public String login() {
+        String username = "zc";
+        String password = "123";
+
+        Subject currentUser = SecurityUtils.getSubject();
+        if (!currentUser.isAuthenticated()) {
+            UsernamePasswordToken token = new UsernamePasswordToken(username, password);
+            token.setRememberMe(true);
+            try {
+                System.out.println(token.hashCode());
+                currentUser.login(token);
+            } catch (AuthenticationException e) {
+                System.out.println("登录失败:"+e.getMessage());
+            }
+
+        }
+        return "succ";
+
+    }
+
+}
--- src/main/java/com/shiro/realm/AuthRealm.java
+++ src/main/java/com/shiro/realm/AuthRealm.java
@@ -3,20 +3,44 @@ package com.shiro.realm;
 import org.apache.shiro.authc.AuthenticationException;
 import org.apache.shiro.authc.AuthenticationInfo;
 import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authc.UsernamePasswordToken;
 import org.apache.shiro.authz.AuthorizationInfo;
 import org.apache.shiro.realm.AuthorizingRealm;
 import org.apache.shiro.subject.PrincipalCollection;
-public class AuthRealm extends AuthorizingRealm{
+
+public class AuthRealm extends AuthorizingRealm {
 
     @Override
     protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principals) {
-        // TODO Auto-generated method stub
+
         return null;
     }
 
     @Override
     protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
-        // TODO Auto-generated method stub
+        System.out.println(token.hashCode());
+        // 把AuthenticationToken转化为UsernamePasswordToken
+        UsernamePasswordToken upToken = (UsernamePasswordToken) token;
+        // 从UsernamePasswordToken获取username
+        String username = upToken.getUsername();
+
+        // 在数据库中查询username
+
+        // 用户不存在,抛出异常
+
+        // 根据用户状况，抛出其他异常(如用户被锁定)
+        
+        
+        //CredentialsMatcher属性比对密码
+
+        // principal-认证信息实体 credentials-密码
+        // realmName-当前realm对象的name,调用父类getName()即可
+        Object principal = username;
+        Object credentials = "123";
+        String realmName = getName();
+        SimpleAuthenticationInfo info = new SimpleAuthenticationInfo(principal, credentials, realmName);
+
         return null;
     }
 
